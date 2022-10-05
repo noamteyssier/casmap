4ec40bc29a1c0672e69180476b3968a004caa1c2
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-10-05 16:03:26 -0700

    determine size of sgRNA in table

--- src/sgrna_table.rs
+++ src/sgrna_table.rs
@@ -1,9 +1,11 @@
-use anyhow::Result;
+use std::{collections::HashSet, hash::Hash};
+
+use anyhow::{Result, bail};
 use serde::{Serialize, Deserialize};
 
 
 #[allow(non_camel_case_types)]
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Hash, Eq, PartialEq)]
 pub struct sgRNA {
     
     // DNA sequence of the variable region
@@ -14,9 +16,19 @@ pub struct sgRNA {
 
 }
 
+impl sgRNA {
+    pub fn sequence(&self) -> &str {
+        &self.sequence
+    }
+    pub fn cid(&self) -> usize {
+        self.cid
+    }
+}
+
 #[derive(Debug)]
 pub struct VariableTable {
-    records: Vec<sgRNA>,
+    records: HashSet<sgRNA>,
+    variable_length: usize,
 }
 impl VariableTable {
     pub fn from_file(filepath: &str) -> Result<Self> {
@@ -29,9 +41,29 @@ impl VariableTable {
         let records = reader
             .into_deserialize()
             .filter_map(|x| x.ok())
-            .collect();
+            .fold(HashSet::new(), |mut set, x| {
+                set.insert(x);
+                set
+            });
+        let variable_length = Self::calculate_variable_length(&records)?;
+        Ok(Self{records, variable_length})
+    }
 
-        Ok(Self{records})
-
+    fn calculate_variable_length(seqset: &HashSet<sgRNA>) -> Result<usize> {
+        let len_set = seqset
+            .iter()
+            .map(|x| x.sequence())
+            .map(|x| x.len())
+            .fold(HashSet::new(), |mut set, x| {
+                set.insert(x);
+                set 
+            });
+        if len_set.len() == 1 {
+            Ok(*len_set.iter().next().unwrap())
+        } else if len_set.len() == 0 {
+            bail!("No records found in sgRNA table")
+        } else {
+            bail!("Multiple sequence lengths found in sgRNA table")
+        }
     }
 }
