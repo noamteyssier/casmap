661a40f6f1db7cde0e8fc553e9cb8869e7e4f351
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-10-11 16:58:58 -0700

    added functionality which parses tuples as Rc<Strings> and stores tuple hashmaps for fast hash maps

---
+++ src/tuple_table.rs
@@ -0,0 +1,84 @@
+use std::{rc::Rc, borrow::Borrow};
+use anyhow::Result;
+use hashbrown::HashMap;
+use crate::{spacer::Spacer, utils::reverse_complement};
+
+#[derive(Clone, Debug, Hash, Eq, PartialEq)]
+pub struct SeqWrapper (Rc<String>);
+impl Borrow<str> for SeqWrapper {
+    fn borrow(&self) -> &str {
+        (*self.0).borrow()
+    }
+}
+
+pub type SeqTuple = (
+    SeqWrapper,
+    SeqWrapper,
+    SeqWrapper,
+    SeqWrapper,
+    SeqWrapper,
+    SeqWrapper,
+);
+
+#[derive(Debug)]
+pub struct TupleTable {
+    sequences: HashMap<SeqWrapper, SeqWrapper>,
+    tuple_map: HashMap<SeqTuple, usize>,
+    k: usize,
+}
+impl TupleTable {
+    pub fn from_file(filepath: &str) -> Result<Self> {
+        let mut reader = csv::ReaderBuilder::new()
+            .has_headers(false)
+            .delimiter(b'\t')
+            .flexible(false)
+            .from_path(filepath)?;
+
+        let mut records = reader.deserialize().filter_map(|x| x.ok()).collect::<Vec<Spacer>>();
+        records.sort_unstable_by(|a, b| a.ordering(&b));
+
+        let mut sequences = HashMap::with_capacity(records.len());
+        let mut tuple_map = HashMap::with_capacity(records.len());
+        for chunks in records.chunks(6) {
+            let cid = chunks[0].cid();
+            let record_sequences = chunks
+                .iter()
+                .map(|x| Rc::new(x.sequence().to_string()))
+                .map(|x| SeqWrapper(x))
+                .collect::<Vec<SeqWrapper>>();
+
+            let tuple = (
+                record_sequences[0].clone(),
+                record_sequences[1].clone(),
+                record_sequences[2].clone(),
+                record_sequences[3].clone(),
+                record_sequences[4].clone(),
+                record_sequences[5].clone()
+                );
+            tuple_map.insert(tuple, cid);
+
+            record_sequences
+                .iter()
+                .map(|x| (x, reverse_complement(x.borrow())))
+                .map(|(x, r)| (x.clone(), SeqWrapper(Rc::new(r.to_string()))))
+                .for_each(|(x, r)| {
+                    sequences.insert(x.clone(), x.clone());
+                    sequences.insert(r, x);
+                });
+        }
+        let k = records[0].sequence().len();
+        Ok(Self{sequences, tuple_map, k})
+    }
+    pub fn k(&self) -> usize {
+        self.k
+    }
+    pub fn len(&self) -> usize {
+        self.tuple_map.len()
+    }
+    pub fn get_spacer(&self, seq: &str) -> Option<SeqWrapper> {
+        self.sequences.get(seq).map(|x| x.clone())
+    }
+    pub fn get_tuple(&self, tuple: &SeqTuple) -> Option<usize> {
+        self.tuple_map.get(tuple).map(|x| *x)
+    }
+}
